d98a132372dd64da3bfce08ee9b55137e6dba9d7
Aleksandr-Bondarenko <slenderman.dnp@gmail.com>
2021-08-06 03:39:25 +0300

    adds colors switch

--- src/app.js
+++ src/app.js
@@ -1,2 +1,38 @@
-console.log("Hello");
-console.log("New build");
+const colorsData = require("./colors-data");
+const randomInteger = require("./random-generator");
+
+const body = document.querySelector("body");
+const startBtn = document.querySelector("button");
+const stopBtn = startBtn.nextElementSibling;
+
+let timerId = null;
+
+startBtn.style.cursor = "pointer";
+stopBtn.style.cursor = "not-allowed";
+
+const onStartClick = () => {
+  console.log("click on start");
+
+  timerId = setInterval(changeBgc, 1000);
+  stopBtn.addEventListener("click", onStopClick);
+  startBtn.removeEventListener("click", onStartClick);
+  startBtn.style.cursor = "not-allowed";
+  stopBtn.style.cursor = "pointer";
+};
+
+const onStopClick = () => {
+  console.log("click on stop");
+  clearInterval(timerId);
+  stopBtn.removeEventListener("click", onStopClick);
+  startBtn.addEventListener("click", onStartClick);
+  startBtn.style.cursor = "pointer";
+  stopBtn.style.cursor = "not-allowed";
+};
+
+const changeBgc = () => {
+  const randomIndex = randomInteger(0, colorsData.length - 1);
+  body.style.backgroundColor = colorsData[randomIndex];
+};
+
+startBtn.addEventListener("click", onStartClick);
+stopBtn.addEventListener("click", onStopClick);
